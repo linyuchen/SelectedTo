7e020516852bdf5622657e4b83ae2a35189e06de
linyuchen <lin.yu.chen@foxmail.com>
2023-11-26 11:42:28 +0800

    hotkey

--- WinDLL/TestDLL/TestDLL.cpp
+++ WinDLL/TestDLL/TestDLL.cpp
@@ -1,33 +1,57 @@
 ﻿#include <windows.h>
 #include <iostream>
+#include <thread>
+#include <map>
+#include <queue>
+#include <mutex>
 using namespace std;
+typedef void(*FunctionPtr)();
+typedef char* (*CopySelectedTextFunc)();  // 根据函数的参数和返回值类型定义函数指针类型
+typedef BOOL(*AddHotKeyFunc)(int modkey, char key, FunctionPtr func);
+CopySelectedTextFunc CopySelectedText;
+AddHotKeyFunc AddHotKey;
+
+
+void getSelectedText() {
+	char* selectedText = CopySelectedText();
+	if (selectedText != NULL) {
+		cout << "获取复制的内容:" << selectedText << endl;
+	}
+	else {
+		cout << "没有复制文本" << endl;
+	}
+}
 
 int main() {
+
+    
 	HINSTANCE hDLL = LoadLibrary(TEXT("GetSelected.dll"));
 	if (hDLL == NULL) {
 		// DLL加载失败的处理代码
 		cout << "加载dll失败";
 	}
 	else {
-		typedef char* (*CopySelectedTextType)();  // 根据函数的参数和返回值类型定义函数指针类型
 
-		CopySelectedTextType CopySelectedText = (CopySelectedTextType)GetProcAddress(hDLL, "CopySelectedText");
-		if (CopySelectedText == NULL) {
+		CopySelectedText = (CopySelectedTextFunc)GetProcAddress(hDLL, "CopySelectedText");
+		AddHotKey = (AddHotKeyFunc)GetProcAddress(hDLL, "AddHotKey");
+		if (AddHotKey == NULL) {
 			// 获取函数地址失败的处理代码
-			cout << "获取函数CopySelectedText失败";
+			cout << "获取函数AddHotKey失败";
 		}
 		else {
-			cout << "五秒后复制鼠标选中文本" << endl;
-			Sleep(5000);
-			char* selectedText = CopySelectedText();
-			if (selectedText != NULL) {
-				cout << "获取复制的内容:" << selectedText << endl;
+			if (CopySelectedText == NULL) {
+				// 获取函数地址失败的处理代码
+				cout << "获取函数CopySelectedText失败";
 			}
-			else{
-				cout << "没有复制文本" << endl;
+			else {
+				AddHotKey(MOD_ALT, 'D', getSelectedText);
 			}
 		}
+		string s;
+		cout << "press anykey to quit" << endl;
+		cin >> s;
 		FreeLibrary(hDLL);
+		
 	}
 }
 
--- WinDLL/dllmain.cpp
+++ WinDLL/dllmain.cpp
@@ -3,18 +3,90 @@
 #include <Windows.h>
 #include <fstream>
 #include <iostream>
+#include <thread>
+#include <map>
+#include <queue>
+#include <mutex>
+
+typedef void(*FunctionPtr)();
 using namespace std;
 
 #define DLL_EXPORT __declspec(dllexport)
+
+int globalHotKeyMsgId = 998;
+map<int, FunctionPtr> hotkeyIds;
+std::mutex mtx;
+
+
+
+class HotKeyInfo
+{
+public:
+    int hotkeyMsgId;
+    int modId;
+    char key;
+
+    HotKeyInfo(int hotkeyMsgId, int modId, char key)
+        : hotkeyMsgId(hotkeyMsgId), modId(modId), key(key) {
+        
+    }
+};
+
+queue<HotKeyInfo> hotkeyQueue;
+
+
+void HotKeyThread()
+{
+    unique_lock<std::mutex> qlock(mtx, defer_lock);
+    
+    while (true) 
+    {
+        MSG msg = { 0 };
+        while (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE) != 0)
+        {
+            if (msg.message == WM_HOTKEY)
+            {
+                int hotkeyMsgId = msg.wParam;
+                if (hotkeyIds.find(hotkeyMsgId) != hotkeyIds.end()) {
+                    FunctionPtr callback = hotkeyIds[hotkeyMsgId];
+                    callback();
+                }
+            }
+        }
+        
+        qlock.lock();
+        while (!hotkeyQueue.empty())
+        {
+            HotKeyInfo hotkeyInfo = hotkeyQueue.front();
+            hotkeyQueue.pop();
+            if (!RegisterHotKey(NULL, hotkeyInfo.hotkeyMsgId, hotkeyInfo.modId, hotkeyInfo.key))
+            {
+                cout << "Failed to register hot key " << hotkeyInfo.modId << hotkeyInfo.key << ". Error: " << GetLastError() << std::endl;
+            }
+        }
+        qlock.unlock();
+        Sleep(100);
+    }
+}
+
+
+
 
 BOOL APIENTRY DllMain( HMODULE hModule,
                        DWORD  ul_reason_for_call,
                        LPVOID lpReserved
                      )
-{
+{   
+    
+
+
     switch (ul_reason_for_call)
     {
     case DLL_PROCESS_ATTACH:
+    {
+        thread t(HotKeyThread);
+        t.detach();
+    }
     case DLL_THREAD_ATTACH:
     case DLL_THREAD_DETACH:
     case DLL_PROCESS_DETACH:
@@ -26,40 +98,54 @@ BOOL APIENTRY DllMain( HMODULE hModule,
 
 
 extern "C" {
+    DLL_EXPORT void AddHotKey(int modId, char key, FunctionPtr callback)
+    {
+        globalHotKeyMsgId++;
+        std::lock_guard<std::mutex> lock(mtx);
+        hotkeyIds[globalHotKeyMsgId] = callback;
+        hotkeyQueue.push(HotKeyInfo(globalHotKeyMsgId, modId, key));
+    }
     DLL_EXPORT char* CopySelectedText() {
         // 获取当前活动窗口的句柄
         HWND hWindow = GetForegroundWindow();
         
-        // 模拟按下和释放Ctrl键
-        keybd_event(VK_CONTROL, 0, 0, 0);
-        keybd_event(VkKeyScan('C'), 0, 0, 0);
+        for (int i = 0; i < 3; i++) {
+            // 模拟按下和释放Ctrl键
+            keybd_event(VK_CONTROL, 0, 0, 0);
+            keybd_event(VkKeyScan('C'), 0, 0, 0);
+            Sleep(100);
+            // 模拟释放Ctrl键
+            keybd_event(VK_CONTROL, 0, KEYEVENTF_KEYUP, 0);
+            keybd_event(VkKeyScan('C'), 0, KEYEVENTF_KEYUP, 0);
 
-        // 模拟释放Ctrl键
-        keybd_event(VK_CONTROL, 0, KEYEVENTF_KEYUP, 0);
-        keybd_event(VkKeyScan('C'), 0, KEYEVENTF_KEYUP, 0);
+            // 将Ctrl + C消息发送给活动窗口
+            SendMessage(hWindow, WM_COPY, 0, 0);
+            Sleep(100);
+        }
+        for (int i = 0; i < 3; i++) {
+            if (OpenClipboard(NULL)) {
+                HANDLE hData = GetClipboardData(CF_TEXT);
+                if (hData != NULL) {
+                    CloseClipboard();
+                    UINT format = EnumClipboardFormats(0);
+                    bool isTextFormatAvailable = IsClipboardFormatAvailable(CF_TEXT);
 
-        // 将Ctrl + C消息发送给活动窗口
-        SendMessage(hWindow, WM_COPY, 0, 0);
-        Sleep(500);
-        if (OpenClipboard(NULL)) {
-            HANDLE hData = GetClipboardData(CF_TEXT);
-            if (hData != NULL) {
-                CloseClipboard();
-                UINT format = EnumClipboardFormats(0);
-                bool isTextFormatAvailable = IsClipboardFormatAvailable(CF_TEXT);
-
-                if (isTextFormatAvailable) {
-                    char* pszData = static_cast<char*>(GlobalLock(hData));
-                    if (pszData != NULL) {
-                        GlobalUnlock(hData);
-                        return pszData;
+                    if (isTextFormatAvailable) {
+                        char* pszData = static_cast<char*>(GlobalLock(hData));
+                        if (pszData != NULL) {
+                            GlobalUnlock(hData);
+                            return pszData;
+                        }
+                    }
+                    else {
+                        //cout << "剪切板中没有文本" << endl;
                     }
                 }
-                else {
-                    //cout << "剪切板中没有文本" << endl;
-                }
+                CloseClipboard();
             }
+            Sleep(100);
         }
+
         return NULL;
     }
 }
